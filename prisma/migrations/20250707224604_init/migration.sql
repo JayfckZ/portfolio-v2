1e492ae3680af576c7e8a47e1c6a83aa6c074dfa
Jay <joaomarcelo1015ja@gmail.com>
2025-07-07 20:40:29 -0300

    setup e conexao com banco de dados

---
+++ prisma/migrations/20250707224604_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagemUrl" TEXT,
+    "repoUrl" TEXT NOT NULL,
+    "deployUrl" TEXT,
+    "tecnologias" TEXT[],
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
